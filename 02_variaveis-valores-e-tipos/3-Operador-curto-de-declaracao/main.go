019b8f57f4d45422bce4fbf01dc9382de5753ef6
gustavoo151 <josegustavooliveira121@gmail.com>
2025-08-03 04:28:54 -0300

    Atribuições de tipo e inferencia

---
+++ 02_variaveis-valores-e-tipos/3-Operador-curto-de-declaracao/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 10.0
+	y := "Olá Bom dia"
+
+	fmt.Printf("X: %v, %T\n", x, x) // O T serve para mostrar o tipo
+	fmt.Printf("Y: %v, %T\n", y, y)
+}
